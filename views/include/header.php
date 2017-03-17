2fe26d039e56ad24eecd6e64539a67cdf13b1e45
BrayantGb <bgamboa47@misena.edu.co>
2017-03-17 12:45:58 -0500

    Login
    HTML del login

---
+++ views/include/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html >
+  <head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/e.css">
+    <script rel="stylesheet" href="js/js.js"></script>
+    </head>
+    <body>
---
+++ views/pages/login.php
@@ -0,0 +1,18 @@
+<img src="imagenes/Logo.png" id="logo">
+<img  src="imagenes/1fit.png" style="width:100%" id="img2">
+<img src="imagenes/triangulo.png" id="triangulo" style="width: 100%">
+<form>
+  <div class="form-group" id="campo1">
+    <input type="email" class="form-control" placeholder="Email" required="Este campo es obligatorio">
+    <span class="glyphicon glyphicon-user" aria-hidden="true" id="contrasena"></span>
+  </div>
+  <div class="form-group">
+    <input type="password" class="form-control" placeholder="Contraseña" required="Este campo es obligatorio">
+    <span class="glyphicon glyphicon-lock" aria-hidden="true" id="contrasena"></span>
+  </div>
+</form>
+<button  type="button" class="btn btn-primary btn-lg btn-block">INICIAR SESIÓN</button>
+  <p>
+    <button  type="button" class="btn btn-default btn-lg" id="btn1">REGISTRATE</button>
+    <button  type="button" class="btn btn-default btn-lg" id="btn2">RECUPERAR CUENTA</button>
+  </p>
