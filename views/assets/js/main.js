c5fb397e25ca7726841bd2f8fbbef21a3e7a8004
BrayantGb <bgamboa47@misena.edu.co>
2017-03-19 17:59:46 -0500

    Dashboard
    Correción

--- views/assets/js/main.js
+++ views/assets/js/main.js
@@ -21,22 +21,3 @@ function openNav() {
      document.getElementById("main").style.marginLeft= "0";
      document.body.style.backgroundColor = "white";
  }
-
- var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
- acc[i].onclick = function(){
-     /* Toggle between adding and removing the "active" class,
-     to highlight the button that controls the panel */
-     this.classList.toggle("active");
-
-     /* Toggle between hiding and showing the active panel */
-     var panel = this.nextElementSibling;
-     if (panel.style.display === "block") {
-         panel.style.display = "none";
-     } else {
-         panel.style.display = "block";
-     }
- }
-}
