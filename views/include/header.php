c3cd265c61d45f09cbc36d48c5af13536fa3fac8
BrayantGb <bgamboa47@misena.edu.co>
2017-03-23 20:31:18 -0500

    Dashboard + Folders

---
+++ views/include/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Estudios McLovin</title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <!--<link rel="stylesheet" href="views/assets/css/bootstrap.min.css">-->
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="views/assets/css/main.css">
+    </head>
+    <body>
