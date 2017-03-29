9c76752548f6b79499bfd346a5623c279c71b92e
Pablo Restrepo <jprestrepo94@misena.edu.co>
2017-03-29 14:24:10 -0500

    Js

--- views/pages/dashboard.php
+++ views/pages/dashboard.php
@@ -1,12 +1,11 @@
-</head>
-<body>
+  <h1>BIENVENIDO <?php echo $_SESSION["usuario"]; ?></h1>
   <nav class=" navbar-inverse">
     <div class="container-fluid">
       <div class="navbar-header">
         <a class="navbar-brand">Mentalidad Fiteness</a>
       </div>
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Log Out</a></li>
+        <li><a href="?c=main&a=close"><span class="glyphicon glyphicon-log-in"></span> Log Out</a></li>
       </ul>
     </div>
   </nav>
@@ -25,27 +24,27 @@
     -->
   		<div class="collapse navbar-collapse" id="bs-sidebar-navbar-collapse-1">
   			<ul class="nav navbar-nav">
-  				<li class=""><a href="#">INICIO<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-home"></span></a></li>
+  				<li class=""><a href="?c=main&a=dashboard">INICIO<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-home"></span></a></li>
           <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">COMIDAS <span class="caret"></span><span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-cutlery"></span></a>
             <ul class="dropdown-menu forAnimate" role="menu">
-              <li><a href="#">Gestionar Alimentos</a></li>
-              <li><a href="#">Gestionar Desayunos</a></li>
-              <li><a href="#">Gestionar Almuerzos</a></li>
-              <li><a href="#">Gestionar Snacks</a></li>
-              <li><a href="#">Gestionar Cenas</a></li>
-              <li><a href="#">Dietas</a></li>
+              <li><a href="?c=alimento">Gestionar Alimentos</a></li>
+              <li><a href="?c=desayuno">Gestionar Desayunos</a></li>
+              <li><a href="?c=almuerzo">Gestionar Almuerzos</a></li>
+              <li><a href="?c=snack">Gestionar Snacks</a></li>
+              <li><a href="?c=comida">Gestionar Cenas</a></li>
+              <li><a href="?c=dieta">Dietas</a></li>
             </ul>
           </li>
           <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">SEGURIDAD <span class="caret"></span><span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-lock"></span></a>
             <ul class="dropdown-menu forAnimate" role="menu">
-              <li><a href="#">Rol</a></li>
-              <li><a href="#">Usuario</a></li>
-              <li><a href="#">Pagina</a></li>
+              <li><a href="?c=rol">Rol</a></li>
+              <li><a href="?c=usuario">Usuario</a></li>
+              <li><a href="?c=pagina">Pagina</a></li>
             </ul>
           </li>
-  				<li ><a href="#">EJERCICIO<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-scale"></span></a></li>
+  				<li ><a href="?c=ejercicio">EJERCICIO<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-scale"></span></a></li>
   			</ul>
   		</div>
   	</div>
