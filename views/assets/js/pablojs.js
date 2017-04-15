a4e0d5e19122c8f43326fec581ae7b0cdfa0fb60
Pablo Restrepo <jprestrepo94@misena.edu.co>
2017-04-14 21:31:16 -0500

    pagina

--- views/assets/js/pablojs.js
+++ views/assets/js/pablojs.js
@@ -1,3 +1,4 @@
+//ROLE
 $("#estadoRol").focus(function(){
   $("#nombreRol").siblings("span").remove();
   var name = $("#nombreRol").val();
@@ -28,6 +29,54 @@ $("#frmrole").submit(function(e){
       if (data[0] === true) {
         alert(data[1]);
         document.location.href="index.php?c=role";
+      }else{
+        alert(data[1]);
+      }
+      //document.location.href="index.php?c=role";
+    })
+  }
+});
+//------------------------------PAGE
+$("#iconoPag").focus(function(){
+  $("#nombrePag").siblings("span").remove();
+  var name = $("#nombrePag").val();
+  $.post("index.php?c=page&a=validPage",{nombre:name},function(data){
+          var data = JSON.parse(data);
+
+          if(data[0] == false){
+            $("#pagaddbut").attr("disabled",true);
+            $("#nombrePag").siblings("label").after("<span class='error'>"+data[1]+"</span>");
+          }else{
+            $("#pagaddbut").attr("disabled",false);
+          }
+  });
+})
+
+$("#nombrePag").focus(function(){
+  $(this).siblings("span").remove();
+})
+
+
+$("#frmpag").submit(function(e){
+  e.preventDefault();
+  if ($(this).parsley().isValid()) {
+    var name = $("#nombrePag").val();
+    var icon = $("#iconoPag").val();
+    var main = $("#menuPag").val();
+    var colors = $("#coloresPag").val();
+    var url = $("#urlPag").val();
+    var photo = $("#fotoPag").val();
+    var description = $("#descripcionPag").val();
+    var button = $("#botonesPag").val();
+    var propierty = $("#propiedadesPag").val();
+    var section = $("#seccionesPag").val();
+    var images = $("#imagenesPag").val();
+    var status = $("#estadoPag").val();
+    $.post("index.php?c=page&a=create",{nombre:name,icono:icon,menu:main,colores:colors,url:url,foto:photo,descripcion:description,boton:button,propiedad:propierty,seccion:section,imagenes:images,estado:status},function(data){
+      var data = JSON.parse(data);
+      if (data[0] === true) {
+        alert(data[1]);
+        document.location.href="index.php?c=page";
       }else{
         alert(data[1]);
       }
