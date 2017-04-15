56e5f5225c5d99569ea1c3a4a6e86996ad68e2ea
Daniel Cardona <decardona34@misena.edu.co>
2017-04-15 17:43:37 -0500

    Recover Password

--- views/modules/security_mod/user_manage/main/recoverPassword.php
+++ views/modules/security_mod/user_manage/main/recoverPassword.php
@@ -3,10 +3,22 @@
 <div class="">
     <form class="" action="index.php?c=user&a=updatePassword" method="post">
         <div class="">
+            <label for="">Vieja contraseña</label>
+        </div>
+        <div class="">
+            <input type="password" name="" value="">
+        </div>
+        <div class="">
             <label for="">Nueva contraseña</label>
         </div>
         <div class="">
             <input type="password" name="data[]" value="">
+        </div>
+        <div class="">
+            <label for="">Confirmar Nueva contraseña</label>
+        </div>
+        <div class="">
+            <input type="password" name="" value="">
         </div>
         <div class="">
             <input type="hidden" readonly value="<?php echo $user['token'];?>" name="data[]">
