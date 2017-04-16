962e1e007970685b926b47960531ae09996b662c
Linincool <juanpablo34561@hotmail.com>
2017-04-16 17:43:20 -0500

    estilos
    actualizacion estilos

--- views/include/header.php
+++ views/include/header.php
@@ -5,8 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Mentalidad Fitness</title>
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-        <!--<link rel="stylesheet" href="views/assets/css/bootstrap.min.css">-->
-          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="views/assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <link rel="stylesheet" href="views/assets/css/main.css">
     </head>
     <body>
