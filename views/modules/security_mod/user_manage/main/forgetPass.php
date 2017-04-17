22659ecac259a339be0e1d510c174573b89a90e3
Linincool <juanpablo34561@hotmail.com>
2017-04-16 22:41:01 -0500

    Formularios_organizados
    formularios con animaciones en todos los inputs, corrección de otros
    errores css

--- views/modules/security_mod/user_manage/main/forgetPass.php
+++ views/modules/security_mod/user_manage/main/forgetPass.php
@@ -1,24 +1,24 @@
-  <body id="bodyMF1">
-    <!-- RECUPERAR CUENTA -->
-    <div class="container" style="position:absolute;z-index:0">
-      <div class="row">
-        <div class="col-xs-12 col-md-4 col-md-offset-4">
-              <img src="views/assets/images/images_principal/LogoMFrojo.png" class="img-responsive" id="MF_logoMF">
-              <form class="" name="form" action="?c=user&a=sendEmailRecoverPassword" method="post" id="MF_formulario" data-parsley-validate>
-                <!-- INPUT EMAIL -->
-                <div class="form-group">
-                  <input type="email" class="form-control" name="email" id="MF_inputemailred" required="" data-parsley-type placeholder="EMAIL">
-                  <!-- <label id="MF_email"><i class="fa fa-envelope" aria-hidden="true"></i> EMAIL</label> -->
-                </div>
-                <!-- BOTON DE ENVIO -->
-                <button type="submit" class="btn btn-default" id="MF_button1">ENVIAR</button>
-                <div class="row">
-                  <div class="col-xs-10 col-xs-offset-1">
-                    <!-- DESCRIPCION -->
-                    <h4 class="text-center large" style="">Por favor digite su email, allí le enviaremos un correo para su nueva contraseña.</h4>
-                  </div>
-                </div>
-              </form>
+<div class="bodyMF1"></div>
+<!-- RECUPERAR CUENTA -->
+<div class="container" style="position:absolute;z-index:0">
+  <div class="row">
+    <div class="col-xs-12 col-md-4 col-md-offset-4">
+      <img src="views/assets/images/images_principal/LogoMFrojo.png" class="img-responsive MF_logoMF">
+        <form class="MF_formulario" name="form" action="?c=user&a=sendEmailRecoverPassword" method="post" data-parsley-validate>
+            <!-- INPUT EMAIL -->
+            <div class="form-group">
+              <input type="text" class="form-control MF_inputred" name="email" required="" data-parsley-type>
+              <label class="MF_emailred"><i class="fa fa-envelope" aria-hidden="true"></i> EMAIL</label>
+            </div>
+            <!-- BOTON DE ENVIO -->
+            <button type="submit" class="btn btn-default MF_button1">ENVIAR</button>
+            <div class="row">
+              <div class="col-xs-10 col-xs-offset-1">
+                <!-- DESCRIPCION -->
+                <h4 class="text-center large" style="">Por favor digite su email, allí le enviaremos un correo para su nueva contraseña.</h4>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     </div>
